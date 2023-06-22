6abcde6d0cf96b946033ac5a1344c7e23fc65eb5
Richard <rahayn@yahoo.com>
2023-06-22 11:10:50 -0400

    Adding javascript to do search filtering on the tables and clean up

--- RichsSnackRack/RichsSnackRack/wwwroot/js/site.js
+++ RichsSnackRack/RichsSnackRack/wwwroot/js/site.js
@@ -6,22 +6,37 @@
 
 
 const downloadButton = document.getElementById('downloandOrderDetailReceiptId');
+const orderLinkToCopy = document.querySelector("#orderDetailLink");
+const searchInput = document.getElementById('searchInput');
+const orderItemRows = document.querySelectorAll("tbody tr");
 
 // TODO the selectors need to have the values passed in so other things can get selected
-document.querySelector("#copyBtnId").addEventListener("click", () => {
-    let orderId = document.querySelector("#orderDetailId").innerHTML;
-    let clipboard = navigator.clipboard;
-    clipboard.writeText(orderId);
-})
+// TODO consider moving these4 functions to their own script files or folders
 
+// Order Confirmation
+if (orderLinkToCopy) {
+    orderLinkToCopy.addEventListener("click", () => {
+        const orderId = document.querySelector("#hiddenOrderDetailId").innerHTML;
+        const clipboard = navigator.clipboard;
+        clipboard.writeText(orderId);
+    });
+};
 
+if (downloadButton) {
+    downloadButton.addEventListener("click", () => {
+        const downloadElement = document.getElementById('orderDetailReceiptId');
+        html2pdf().from(downloadElement).save();
+    });
+};
 
-// Named functions
-
-function downloadReceiptPDF() {
-    // https://pspdfkit.com/blog/2019/html-to-pdf-in-javascript/
-    const downloadElement = document.getElementById('orderDetailReceiptId');
-    html2pdf().from(downloadElement).save();
+// All Orders
+if (searchInput && orderItemRows) {
+    searchInput.addEventListener("keyup", (event) => {
+        const searchEvent = event.target.value.toLowerCase();
+        orderItemRows.forEach((orderItemRow) => {
+            orderItemRow.querySelector("td").textContent.toLocaleLowerCase().startsWith(searchEvent)
+                ? (orderItemRow.style.display = "table-row")
+                : (orderItemRow.style.display = "none")
+        });
+    });
 }
-
-downloadButton.addEventListener("click", downloadReceiptPDF)
