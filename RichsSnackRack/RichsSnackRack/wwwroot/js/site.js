7b116f87ed38e4264a74d96e9377d3efe08a4d66
Richard <rahayn@yahoo.com>
2023-06-25 10:59:50 -0400

    Moved some of the javascript functions to a more specific file

--- RichsSnackRack/RichsSnackRack/wwwroot/js/site.js
+++ RichsSnackRack/RichsSnackRack/wwwroot/js/site.js
@@ -4,39 +4,3 @@
 // Write your JavaScript code.
 
 
-
-const downloadButton = document.getElementById('downloandOrderDetailReceiptId');
-const orderLinkToCopy = document.querySelector("#orderDetailLink");
-const searchInput = document.getElementById('searchInput');
-const orderItemRows = document.querySelectorAll("tbody tr");
-
-// TODO the selectors need to have the values passed in so other things can get selected
-// TODO consider moving these4 functions to their own script files or folders
-
-// Order Confirmation
-if (orderLinkToCopy) {
-    orderLinkToCopy.addEventListener("click", () => {
-        const orderId = document.querySelector("#hiddenOrderDetailId").innerHTML;
-        const clipboard = navigator.clipboard;
-        clipboard.writeText(orderId);
-    });
-};
-
-if (downloadButton) {
-    downloadButton.addEventListener("click", () => {
-        const downloadElement = document.getElementById('orderDetailReceiptId');
-        html2pdf().from(downloadElement).save();
-    });
-};
-
-// All Orders
-if (searchInput && orderItemRows) {
-    searchInput.addEventListener("keyup", (event) => {
-        const searchEvent = event.target.value.toLowerCase();
-        orderItemRows.forEach((orderItemRow) => {
-            orderItemRow.querySelector("td").textContent.toLocaleLowerCase().startsWith(searchEvent)
-                ? (orderItemRow.style.display = "table-row")
-                : (orderItemRow.style.display = "none")
-        });
-    });
-}
