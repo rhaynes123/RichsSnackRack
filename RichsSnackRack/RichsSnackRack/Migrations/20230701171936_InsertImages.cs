f011d447532b2fdf0c7fa64f6a44a0c53063dc45
Richard <rahayn@yahoo.com>
2023-07-01 13:25:49 -0400

    Adding food images to the web root folder and showing them on the ordering and index page

---
+++ RichsSnackRack/RichsSnackRack/Migrations/20230701171936_InsertImages.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RichsSnackRack.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertImages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE Snacks snk SET snk.ImageUrl = 'pizza-slice.png' WHERE snk.`Name` = 'Pizza';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'pepsi-can.jpg' WHERE snk.`Name` = 'Pepsi';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'ham-burger.png' WHERE snk.`Name` = 'Hamburger';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'coke-can.jpg' WHERE snk.`Name` = 'Coke';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'yogurt-cup.jpg' WHERE snk.`Name` = 'Yogurt';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'apple.jpg' WHERE snk.`Name` = 'Apple';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'yogurt-cup.jpg' WHERE snk.`Name` = 'Yogurt';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'banana.jpg' WHERE snk.`Name` = 'Banana';
+                    UPDATE Snacks snk SET snk.ImageUrl = 'turkey-sandwhich.jpg' WHERE snk.`Name` = 'Turkey Sandwhich';");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
