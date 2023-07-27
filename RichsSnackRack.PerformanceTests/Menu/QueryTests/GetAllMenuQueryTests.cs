971267077a1c01878a71af7f27602ab1a5d50901
Richard <rahayn@yahoo.com>
2023-07-27 12:26:48 -0400

    Adding Performance Tests using Benchmark Dotnet and adding baseline tests for GetMenu

---
+++ RichsSnackRack.PerformanceTests/Menu/QueryTests/GetAllMenuQueryTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Microsoft.EntityFrameworkCore;
+using RichsSnackRack.Menu;
+using RichsSnackRack.Persistence;
+/*
+ * https://benchmarkdotnet.org
+ * https://www.briangetsbinary.com/software%20engineering/dotnet/csharp/performance/2022/05/26/benchmarkdotnet-ef-core-vs-ef-6-part-1.html
+ */
+namespace RichsSnackRack.PerformanceTests.Menu.QueryTests
+{
+    [MemoryDiagnoser]
+    [MaxColumn]
+    [MinColumn]
+    public sealed class GetAllMenuQueryTests
+    {
+        private SnackRackDbContext DbContext;
+        public GetAllMenuQueryTests()
+        {
+            DbContextOptionsBuilder<SnackRackDbContext> builder = new();
+            builder
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .EnableSensitiveDataLogging()
+                .EnableDetailedErrors();
+
+            DbContext = new SnackRackDbContext(builder.Options);
+            DbContext.Database.EnsureDeleted();
+            DbContext.Database.EnsureCreated();
+        }
+        [Benchmark(Baseline = true)]
+        public async Task GetAllMenuQueryReturnsFromHandler()
+        {
+            GetAllMenuQuery menuQuery = new();
+            GetAllMenuQueryHandler handler = new(DbContext);
+            await handler.Handle(menuQuery, default);
+        }
+
+    }
+}
+
---
+++ RichsSnackRack.PerformanceTests/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using Microsoft.EntityFrameworkCore;
+using RichsSnackRack.Menu;
+using RichsSnackRack.PerformanceTests.Menu.QueryTests;
+using RichsSnackRack.Persistence;
+using Xunit;
+
+//Console.WriteLine("Hello, World!");
+//BenchmarkSwitcher.FromAssembly(assembly: typeof(QueryTests).Assembly).Run(args);
+
+var summary = BenchmarkRunner.Run<GetAllMenuQueryTests>();
+
